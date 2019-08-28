04997a58bd8adb72d4cef93fc1480b06f32e39fc
stevewhit <swhit114@gmail.com>
2019-08-27 23:29:16 -0400

    Added predictions table

---
+++ Create Table Predictions.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Predictions](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CompanySymbol] [nvarchar](20) NOT NULL,
+	[Date] [date] NOT NULL,
+	[TrainingStartDate] [date] NOT NULL,
+	[TrainingEndDate] [date] NOT NULL,
+	[TrainingParameters] [nvarchar](250) NULL,
+	[Prediction] [nvarchar](100) NOT NULL,
+	[Confidence] [decimal](5, 2) NOT NULL,
+	[Outcome] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Predictions] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
