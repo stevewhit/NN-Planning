049211885e433e0befc5800e98b802c3298855ad
stevewhit <swhit114@gmail.com>
2019-09-04 17:36:45 -0400

    Added log4net and Ninject to application;

---
+++ SANNET/Ninject.cs
@@ -0,0 +1,37 @@
+﻿using Framework.Generic.EntityFramework;
+using log4net;
+using Ninject;
+using Ninject.Modules;
+using SANNET.Business.Repositories;
+using SANNET.Business.Services;
+using SANNET.DataModel;
+using StockMarket.DataModel;
+
+namespace QR.App
+{
+    public class NinjectBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            var context = new EfContext(new SANNETContext());
+
+            Bind<IEfRepository<Company>>().ToMethod(_ => new EfRepository<Company>(context));
+            Bind<IEfRepository<Quote>>().ToMethod(_ => new EfRepository<Quote>(context));
+            Bind<IEfRepository<Prediction>>().ToMethod(_ => new EfRepository<Prediction>(context));
+            Bind<IEfRepository<NetworkConfiguration>>().ToMethod(_ => new EfRepository<NetworkConfiguration>(context));
+            Bind<IDatasetRepository>().ToMethod(_ => new DatasetRepository(context));
+
+            Bind<ICompanyService<Company>>().ToConstructor(_ => new CompanyService<Company>(Kernel.Get<IEfRepository<Company>>())).InThreadScope();
+            Bind<IQuoteService<Quote>>().ToConstructor(_ => new QuoteService<Quote>(Kernel.Get<IEfRepository<Quote>>())).InThreadScope();
+            Bind<IDatasetService>().ToConstructor(_ => new DatasetService(Kernel.Get<IDatasetRepository>())).InThreadScope();
+            Bind<INetworkConfigurationService<NetworkConfiguration>>().ToConstructor(_ => new NetworkConfigurationService<NetworkConfiguration>(Kernel.Get<IEfRepository<NetworkConfiguration>>())).InThreadScope();
+            Bind<IPredictionService<Prediction>>().ToConstructor(_ => new PredictionService<Prediction, Quote, Company, NetworkConfiguration>(Kernel.Get<IEfRepository<Prediction>>(), 
+                                                                                                                                              Kernel.Get<IQuoteService<Quote>>(), 
+                                                                                                                                              Kernel.Get<ICompanyService<Company>>(), 
+                                                                                                                                              Kernel.Get<IDatasetService>(), 
+                                                                                                                                              Kernel.Get<INetworkConfigurationService<NetworkConfiguration>>())).InThreadScope();
+                        
+            Bind<ILog>().ToMethod(_ => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType));
+        }
+    }
+}
--- SANNET/Program.cs
+++ SANNET/Program.cs
@@ -1,16 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using log4net;
+using Ninject;
+using SANNET.Business.Services;
+using SANNET.DataModel;
+using StockMarket.DataModel;
+using System;
+using System.Reflection;
 
 namespace SANNET
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // TODO: Add Log4Net
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            var predictionService = kernel.Get<PredictionService<Prediction, Quote, Company, NetworkConfiguration>>();
+
+            try
+            {
+                predictionService.GenerateAllPredictions();
+                //marketService.UpdateAllCompanyDetailsAsync().Wait();
+            }
+            catch (AggregateException e)
+            {
+                LogRecursive(kernel.Get<ILog>(), e, "Error occured downloading stock details");
+            }
+
+            try
+            {
+                predictionService.AnalyzePredictions();
+                //marketService.UpdateAllCompaniesWithLatestQuotesAsync().Wait();
+            }
+            catch (AggregateException e)
+            {
+                LogRecursive(kernel.Get<ILog>(), e, "Error occured downloading stock data");
+            }
+
+            predictionService.Dispose();
+            kernel.Dispose();
+        }
+
+        /// <summary>
+        /// Logs the inner exceptions of an AggregateException separately.
+        /// </summary>
+        private static void LogRecursive(ILog log, AggregateException e, string message)
+        {
+            foreach (var innerException in e.InnerExceptions)
+            {
+                if (innerException is AggregateException)
+                    LogRecursive(log, innerException as AggregateException, message);
+                else
+                    log.Error($"{message}", innerException);
+            }
         }
     }
 }
